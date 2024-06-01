17c5f3811c7d041b3229196fc9bff9e667f3c5b0
Fahd Laâbi <66067772+fahdlaabi@users.noreply.github.com>
2024-06-01 15:18:04 +0100

    request => formData

--- src/module/base.ts
+++ src/module/base.ts
@@ -1,20 +1,31 @@
 import got from "got";
 import { WhmcsSetupOptions } from "../interface/whmcs.setup.options";
 
+import FormData = require("form-data");
+
 export abstract class BaseModule {
     constructor(private readonly options: WhmcsSetupOptions) {}
 
-    protected async request(methodName: string, options?: any): Promise<any> {
-        options.identifier = this.options.identifier;
-        options.secret = this.options.secret;
-        options.accesskey = this.options.accesskey;
-        options.action = methodName;
-        options.responsetype = "json";
+    protected async request(
+        methodName: string,
+        options?: Record<string, any>
+    ): Promise<any> {
+        const form = new FormData();
+
+        form.append("identifier", this.options.identifier);
+        form.append("secret", this.options.secret);
+        form.append("accesskey", this.options.accesskey);
+        form.append("action", methodName);
+        form.append("responsetype", "json");
+
+        Object.entries(options).forEach(([value, index]) => {
+            form.append(index, value);
+        });
 
         return new Promise(async (resolve, reject) => {
             const res = await got(this.options.apiUrl + "/includes/api.php", {
                 method: "post",
-                form: options,
+                form,
             });
 
             const data = JSON.parse(res.body);
